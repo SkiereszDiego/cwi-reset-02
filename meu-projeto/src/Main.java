60d18b59f4960e60635ed85f42088e92eaffe1ba
Diego Skieresz <diegoskieresz@gmail.com>
2021-02-06 15:04:14 -0300

    Modificando a mensagem de texto

--- meu-projeto/src/Main.java
+++ meu-projeto/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Primeira aula da CWI Reset!");
+        System.out.println("Primeira aula da CWI Reset!Aprendendo Java");
 
     }
 }
