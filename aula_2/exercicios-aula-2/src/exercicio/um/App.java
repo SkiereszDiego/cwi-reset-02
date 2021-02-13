b8c744806036746b1d9ec7188bc88066dccd5705
Diego Skieresz <diegoskieresz@gmail.com>
2021-02-13 17:56:07 -0300

    Condicionais

--- aula_2/exercicios-aula-2/src/exercicio/um/App.java
+++ aula_2/exercicios-aula-2/src/exercicio/um/App.java
@@ -5,6 +5,7 @@ public class App {
         //new chama um construtor de uma classe
         Diretor diretor1 = new Diretor("Lana Wachowski", 30, 20, Genero.FEMININO);
         Diretor diretor2 = new Diretor("Stanley Kubrick", 50, 50, Genero.MASCULINO);
+
         Filme matrix = new Filme("Matrix","Thomas Anderson," +
                 " a computer programmer, is led to fight an underground war" +
                 " against powerful computers who have constructed his entire" +
@@ -12,15 +13,16 @@ public class App {
         Filme theShining = new Filme("The Shining","Jack and his family" +
                 " move into an isolated hotel with a violent past. Living in isolation," +
                 " Jack begins to lose his sanity, which affects his family members.",150,
-                1980,5, diretor2);
+                1980,10, diretor2);
         Filme matrix4 = new Filme("Matrix 4","Matrix is an upcoming American" +
                 " science fiction action film and the fourth installment in The Matrix franchise.",
                 150,1980,5, diretor1);
 
+        Ator pessoa1 = new Ator("Keanu Reaves", 56,2, Genero.MASCULINO);
+
         matrix.reproduzir();
         matrix4.reproduzir();
-
-        Ator pessoa1 = new Ator("Keanu Reaves", 56,2, Genero.MASCULINO);
+        theShining.reproduzir();
 
         pessoa1.imprimeDados();
         diretor1.imprimeDados();
--- aula_2/exercicios-aula-2/src/exercicio/um/Filme.java
+++ aula_2/exercicios-aula-2/src/exercicio/um/Filme.java
@@ -13,11 +13,12 @@ public class Filme {
 
 
     public Filme(String nome, String descricao, Integer duracao, Integer ano, Integer avaliacao, Diretor diretor) {
+        defineAvaliacao(avaliacao);
+        validaNomeEDefineAvaliacao(nome);
         this.nome = nome;
         this.descricao = descricao;
         this.duracao = duracao;
         this.ano = ano;
-        this.avaliacao = avaliacao;
         this.diretor = diretor;
     }
 
@@ -25,6 +26,23 @@ public class Filme {
         System.out.println("Filme: " + this.nome + " Duração: " + this.duracao + " minutos.");
         System.out.println("Nome do Diretor: " + this.diretor.getNome());
         System.out.println("Descrição: " + this.descricao);
+    }
+
+    private void defineAvaliacao(Integer avaliacao){
+
+        if (avaliacao < 1 || avaliacao > 5) {
+            this.avaliacao = 3;
+        } else {
+            this.avaliacao = avaliacao;
+        }
+    }
+
+    private void validaNomeEDefineAvaliacao(String nome) {
+        if ("Batman vs Superman".equals(nome)) {
+            this.avaliacao = 1;
+        } else if ("Clube da luta".equals(nome)) {
+            this.avaliacao = 5;
+        }
     }
 
 }
